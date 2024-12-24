0dccec7cd864dba3883465b7a2970a185e59d4e6
baderalqallaf <baderalqallaf@BADERALQALLE446>
2024-12-24 11:48:04 +0000

    Add project files.

---
+++ TaskCalculatorFunctions/Models/Addition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskCalculatorFunctions.Models
+{
+    internal class Addition
+    {
+        public int Add (int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+
+        public double Add(double u, int y, int z)
+        {
+            return u + y + z;
+        }
+        public int Salary(int basic, int workhours, int hourrate)
+        {
+            return basic + (workhours * hourrate);    
+        }
+    }
+}
---
+++ TaskCalculatorFunctions/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskCalculatorFunctions.Models;
+
+Console.WriteLine("Hello, World!");
+
+int x, y, z, workhours, hourrate, basic;
+double u;
+Addition addition = new Addition();
+
+Console.WriteLine("Enter number X = ");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number Y = ");
+y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total is = " + addition.Add(x, y));
+Console.WriteLine("------------------------");
+
+Console.WriteLine("Enter number Z = ");
+z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total is = " + addition.Add(x, y, z));
+Console.WriteLine("------------------------");
+
+Console.WriteLine("Enter number Z = ");
+u = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total is = " + addition.Add(u, y, z));
+Console.WriteLine("------------------------");
+
+Console.WriteLine("Enter basic salary : ");
+basic = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter extra work hours :  ");
+workhours = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Hourly rate : ");
+hourrate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total Salary This Month = " + addition.Salary(basic, workhours, hourrate));    
+
